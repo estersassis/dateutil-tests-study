22eb0e91b9e338f7669ffdf59d089e05ad41b956
mp022 <mpferreiradesouza@gmail.com>
2026-06-15 22:10:54 -0300

    Iteration Status: FAILED - FAILED tests/test_relativedelta.py::test_weeks_flo

--- tests/test_relativedelta.py
+++ tests/test_relativedelta.py
@@ -1,1 +1,6 @@
 # Write tests for the relativedelta class here
+import datetime, pytest
+from src.relativedelta import *
+
+def test_weeks_float_result():
+    assert relativedelta.weeks(datetime(1,7,2026)) == 1
