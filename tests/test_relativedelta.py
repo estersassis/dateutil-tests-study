b570e4ef6cca8a095f5b1ae4ffe08dc311012058
Ricardo Kiyoshi Sasaki <ricardo.k.sasaki@gmail.com>
2026-06-16 12:36:11 -0300

    Iteration Status: PASSED

--- tests/test_relativedelta.py
+++ tests/test_relativedelta.py
@@ -11,6 +11,18 @@ class TestDefaults:
     def test_is_falsy(self):
         assert not relativedelta()
 
+    def test_default_attrs(self):
+        subject = relativedelta()
+
+        assert subject.years == 0
+        assert subject.months == 0
+        assert subject.days == 0
+        assert subject.leapdays == 0
+        assert subject.hours == 0
+        assert subject.minutes == 0
+        assert subject.seconds == 0
+        assert subject.microseconds == 0
+
 
 
 
