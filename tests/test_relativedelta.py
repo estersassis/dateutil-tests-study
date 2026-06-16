9747ab58964a3a2bf902d346ad6b430b1151c191
mp022 <mpferreiradesouza@gmail.com>
2026-06-15 22:26:21 -0300

    Iteration Status: FAILED - FAILED tests/test_relativedelta.py::test_inputs_wi

--- tests/test_relativedelta.py
+++ tests/test_relativedelta.py
@@ -5,8 +5,8 @@ from src.relativedelta import *
 def test_weeks_float_result():
     date = relativedelta(datetime.datetime(2026,1,10), datetime.datetime(2026,1,1))
     assert date.weeks == 1
-    date = relativedelta(datetime.datetime(2026,1,25), datetime.datetime(2026,1,1))
-    assert date.weeks == 3
+    date = relativedelta(datetime.datetime(2026,1,25), datetime.datetime(2026,1,14))
+    assert date.weeks == 1
 
 def test_inputs_with_hours():
     date = relativedelta(None, None, 2026, 1, 7, 0, 0, 12, 20, 0, 0, 2026, 2, 7, 0, 0, 12, 40, 0, 0)
