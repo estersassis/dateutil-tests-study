4ec5d378a5fd3dd4f30487edd9530823af72ddfd
mp022 <mpferreiradesouza@gmail.com>
2026-06-15 22:16:56 -0300

    Iteration Status: PASSED

--- tests/test_relativedelta.py
+++ tests/test_relativedelta.py
@@ -3,5 +3,5 @@ import datetime, pytest
 from src.relativedelta import *
 
 def test_weeks_float_result():
-    date = relativedelta(datetime.datetime(2026,1,7), datetime.datetime(2026,1,1))
+    date = relativedelta(datetime.datetime(2026,1,8), datetime.datetime(2026,1,1))
     assert date.weeks == 1
