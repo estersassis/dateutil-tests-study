31d48f7d0d038e1e0939a79fcb1d420b23a8baf7
zzzluquinhas <lucassouza1965@gmail.com>
2026-06-17 12:15:58 -0300

    Iteration Status: PASSED

--- tests/test_relativedelta.py
+++ tests/test_relativedelta.py
@@ -55,9 +55,14 @@ def test_relativedelta_should_throw_on_invalid_yearday():
 # formatting functions
 
 def test_relativedelta_fix_should_div_correctly():
-	rd = relativedelta(years=1, months=11, hours=23, minutes=59, seconds=59, microseconds=1000000)
+	rd = relativedelta(years=1, months=12, hours=23, minutes=59, seconds=59, microseconds=1000000)
 
-	assert rd.microseconds == 0 and rd.seconds == 0 and rd.minutes == 0 and rd.hours == 0 and rd.months == 0 and rd.years == 2
+	assert rd.years == 2
+	assert rd.months == 0
+	assert rd.hours == 0
+	assert rd.minutes == 0
+	assert rd.seconds == 0
+	assert rd.microseconds == 0
 
 def test_relativedelta_should_normalize():
 	rd = relativedelta(days=3.83771875)
@@ -109,3 +114,11 @@ def test_relativedelta_should_divide():
 	assert result.years == 5
 	assert result.months == 3
 	assert result.days == 1
+
+def test_relativedelta_should_abs():
+	rd = relativedelta(years=-10, months=-7, days=-3)
+	result = abs(rd)
+
+	assert result.years == 10
+	assert result.months == 7
+	assert result.days == 3
